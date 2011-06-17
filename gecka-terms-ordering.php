99940b6702f23688c6d66dd50940ba4e374d2bcc
Laurent Dinclaux <lox.dev@knc.nc>
2011-06-18 09:33:37 +1100

    First commit

---
+++ gecka-terms-ordering.php
@@ -0,0 +1,372 @@
+<?php
+/*
+Plugin Name: Gecka Terms Ordering
+Plugin URI: http://gecka-apps.com
+Description: Order you categories, tags or any other taxonomy of your Wordpress website
+Version: 1.0-beta1
+Author: Gecka
+Author URI: http://gecka.nc
+Licence: GPL
+*/
+
+$gecka_term_ordering = Gecka_Terms_Ordering::instance();
+
+class Gecka_Terms_Ordering {
+
+    private static $instance;
+
+    private static $taxonomies = array('category');
+    
+    private static $plugin_url;
+    private static $plugin_path;
+
+    private function __construct () {
+
+    	self::$plugin_url 	= plugins_url('', __FILE__);
+    	self::$plugin_path 	= dirname(__FILE__);
+
+    	register_activation_hook( __FILE__, array( $this, 'activation_hook' ) );
+    	
+    	add_action( 'plugins_loaded', array($this, 'plugins_loaded'), 5 );
+    	add_action( 'after_setup_theme', array($this, 'after_setup_theme'), 5 );
+    	
+    	add_action( 'init', array($this, 'metadata_wpdbfix') );
+    	add_action( 'switch_blog', array($this, 'metadata_wpdbfix') );
+    	
+    	add_action( 'admin_init', array($this, 'admin_init') );
+    
+		add_filter( 'terms_clauses', array( $this, 'terms_clauses') , 10, 3 );
+    	
+    	add_action( 'created_term', array($this, 'created_term') , 10, 3 );
+    	
+    	add_action( 'delete_term', array($this, 'delete_term') , 10, 3 );
+    	
+    }
+    
+    public static function instance () {
+
+    	if ( ! isset( self::$instance ) ) {
+            $class_name = __CLASS__;
+            self::$instance = new $class_name;
+        }
+
+        return self::$instance;
+        
+    }
+    
+    /**
+     * Add custom ordering support to one or more taxonomies
+     * 
+     * @param string|array $taxonomy 
+     */
+    public static function add_taxonomy_support($taxonomy) {
+    
+    	$taxonomies = (array)$taxonomy ;
+    	self::$taxonomies = array_merge(self::$taxonomies, $taxonomies);
+    	
+    }
+    
+	/**
+     * Add custom ordering support to one or more taxonomies
+     * 
+     * @param string|array $taxonomy 
+     */
+    public static function remove_taxonomy_support($taxonomy) {
+		
+    	$key = array_search ($taxonomy, self::$taxonomies);
+    	if( false !== $key ) unset( self::$taxonomies[$key] );
+    	
+    }
+    
+	public static function has_support ($taxonomy) {
+		
+		if( in_array($taxonomy, self::$taxonomies) ) return true;
+		return false;
+		
+	}
+    
+    /**
+     * Hooks and filters
+     */
+	public function activation_hook () {
+	    
+		if (version_compare(PHP_VERSION, '5.0.0', '<')) {
+	        deactivate_plugins( basename(dirname(__FILE__)) . '/' . basename(__FILE__) ); // Deactivate ourself
+	        wp_die("Sorry, the Gecka Terms Ordering plugin requires PHP 5 or higher.");
+	    }
+	    
+	    global $wpdb;
+	    
+	    $collate = '';
+	    if($wpdb->supports_collation()) {
+			if(!empty($wpdb->charset)) $collate = "DEFAULT CHARACTER SET $wpdb->charset";
+			if(!empty($wpdb->collate)) $collate .= " COLLATE $wpdb->collate";
+	    }
+	    
+	    $sql = "CREATE TABLE IF NOT EXISTS ". $wpdb->prefix . "termmeta" ." (
+	            `meta_id` bigint(20) unsigned NOT NULL auto_increment,
+	            `term_id` bigint(20) unsigned NOT NULL default '0',
+	            `meta_key` varchar(255) default NULL,
+	            `meta_value` longtext,
+	            PRIMARY KEY (meta_id),
+	            KEY term_id (term_id),
+	            KEY meta_key (meta_key) ) $collate;";
+	    $wpdb->query($sql);
+	    
+	}	
+    
+	public function plugins_loaded () {
+		self::$taxonomies = apply_filters( 'term-ordering-default-taxonomies', self::$taxonomies );
+	}
+	
+ 	public function after_setup_theme () {
+    	self::$taxonomies = apply_filters( 'term-ordering-taxonomies', self::$taxonomies );
+    }
+    
+    public function admin_init () {
+
+    	// Load needed scripts to order terms
+    	add_action( 'admin_footer-edit-tags.php', array($this, 'admin_enqueue_scripts'), 10 );
+
+    	add_action( 'admin_print_styles-edit-tags.php', array($this, 'admin_css'), 1 );
+    	
+    	// Httpr hadler for drag and drop ordering
+    	add_action( 'wp_ajax_terms-ordering', array($this, 'terms_ordering_httpr') );
+    	
+    }
+    
+    public function metadata_wpdbfix () {
+    	
+    	global $wpdb;
+	  	$wpdb->termmeta = "{$wpdb->prefix}termmeta";
+	  	
+    }
+    
+    /**
+ 	 * Load needed scripts to order categories in admin
+ 	 */
+	public function admin_enqueue_scripts () {
+
+		if( ! isset( $_GET['taxonomy'] ) || ! self::has_support( $_GET['taxonomy'] ) ) return;
+		
+		wp_register_script( 'gecka-terms-ordering', self::$plugin_url . '/javascripts/terms-ordering.js', array('jquery-ui-sortable'));
+		wp_enqueue_script( 'gecka-terms-ordering' );
+
+		wp_localize_script( 'gecka-terms-ordering', 'terms_order', array( 'taxonomy'=>$_GET['taxonomy'] ) );
+		
+		wp_print_scripts('gecka-terms-ordering');
+	}
+	
+	public function admin_css () {
+		if( ! isset( $_GET['taxonomy'] ) || ! self::has_support( $_GET['taxonomy'] ) ) return;
+		
+		?>
+<style type="text/css">
+	.widefat  .product-cat-placeholder { outline: 1px dotted #21759B; height: 60px; }
+</style>
+		<?php 
+	}
+	/**
+	 * Httpr handler for categories ordering
+	 */
+	public function terms_ordering_httpr () {
+	
+		global $wpdb;
+		
+		$id = (int)$_POST['id'];
+		$next_id  = isset($_POST['nextid']) && (int) $_POST['nextid'] ? (int) $_POST['nextid'] : null;
+		$taxonomy = isset($_POST['taxonomy']) && $_POST['taxonomy'] ? $_POST['taxonomy'] : null;
+		
+		if( ! $id || ! $term = get_term_by('id', $id, $taxonomy) ) die(0);
+		
+		$this->place_term ( $term, $taxonomy, $next_id );
+		
+		$children = get_terms( $taxonomy, "child_of=$id&menu_order=ASC&hide_empty=0" );
+		
+		if( $term && sizeof($children) ) {
+			'children';
+			die;	
+		}
+		
+	}
+		
+	/**
+	 * Add term ordering suport to get_terms, set it as default
+	 * 
+	 * It enables the support a 'menu_order' parameter to get_terms for the configured taxonomy.
+	 * By default it is 'ASC'. It accepts 'DESC' too
+	 * 
+	 * To disable it, set it ot false (or 0)
+	 * 
+	 */
+	public function terms_clauses($clauses, $taxonomies, $args ) {
+		
+		global $wpdb;
+		
+		$taxonomies = (array)$taxonomies;
+		if( sizeof( $taxonomies === 1 ) ) $taxonomy = array_shift( $taxonomies );
+		else return $clauses;
+		
+		if( !$this->has_support($taxonomy) ) return $clauses;
+		
+		// fields
+		if( strpos('COUNT(*)', $clauses['fields']) === false ) $clauses['fields']  .= ', tm.meta_key, tm.meta_value ';
+	
+		// join
+		$clauses['join'] .= " LEFT JOIN {$wpdb->termmeta} AS tm ON (t.term_id = tm.term_id AND tm.meta_key = 'order') ";
+		
+		// order
+		if( isset($args['menu_order']) && ! $args['menu_order']) return $clauses; // menu_order is false whe do not add order clause
+		
+		// default to ASC
+		if( ! isset($args['menu_order']) || ! in_array( strtoupper($args['menu_order']), array('ASC', 'DESC')) ) $args['menu_order'] = 'ASC';
+	
+		$order = "ORDER BY CAST(tm.meta_value AS SIGNED) " . $args['menu_order'];
+		
+		if ( $clauses['orderby'] )
+			$clauses['orderby'] = str_replace ('ORDER BY', $order . ',', $clauses['orderby'] );
+		else
+			$clauses['orderby'] = $order;
+		
+		return $clauses;
+	
+	}
+	
+	/**
+	 * Reorder on term insertion
+	 * 
+	 * @param int $term_id
+	 */
+	public function created_term ($term_id, $tt_id, $taxonomy) {
+		
+		if( !$this->has_support($taxonomy) ) return;
+		
+		$next_id = null;
+		
+		$term = get_term($term_id, $taxonomy);
+		
+		// gets the sibling terms
+		$siblings = get_terms($taxonomy, "parent={$term->parent}&menu_order=ASC&hide_empty=0");
+		
+		foreach ($siblings as $sibling) {
+			if( $sibling->term_id == $term_id ) continue;
+			$next_id =  $sibling->term_id; // first sibling term of the hierachy level
+			break;
+		}
+	
+		// reorder
+		$this->place_term ( $term, $taxonomy, $next_id );
+		
+	}
+	
+	
+	/**
+	 * Delete terms metas on deletion
+	 * 
+	 * @param int $term_id
+	 */
+	public function delete_term ($term_id, $tt_id, $taxonomy) {
+		
+		if( !$this->has_support($taxonomy) ) return;
+		
+		if( !(int)$term_id ) return;
+		
+		global $wpdb;
+		$wpdb->query( "DELETE FROM {$wpdb->_termmeta} WHERE `_term_id` = " . (int) $term_id );
+		
+	}
+	
+	/**
+	 * Move a term before the a	given element of its hierachy level
+	 *
+	 * @param object $the_term
+	 * @param int $next_id the id of the next slibling element in save hierachy level
+	 * @param int $index
+	 * @param int $terms
+	 */
+	private function place_term ( $the_term, $taxonomy, $next_id, $index=0, $terms=null ) {
+		
+		if( ! $terms ) $terms = get_terms($taxonomy, 'menu_order=ASC&hide_empty=0&parent=0');
+		if( empty( $terms ) ) return $index;
+		
+		$id	= $the_term->term_id;
+		
+		$term_in_level = false; // flag: is our term to order in this level of terms
+		
+		foreach ( $terms as $term ) {
+			
+			if( $term->term_id == $id ) { // our term to order, we skip
+				$term_in_level = true;
+				continue; // our term to order, we skip
+			}
+			// the nextid of our term to order, lets move our term here
+			if( null !== $next_id && $term->term_id == $next_id ) { 
+				$index = $this->set_term_order( $id, $taxonomy, $index+1, true );
+			}		
+			
+			// set order
+			$index = $this->set_term_order( $term->term_id, $taxonomy, $index+1 );
+			
+			// if that term has children we walk thru them
+			$children = get_terms($taxonomy, "parent={$term->term_id}&menu_order=ASC&hide_empty=0");
+			if( !empty($children) ) {
+				$index = $this->place_term( $the_term, $taxonomy, $next_id, $index, $children );	
+			}
+		}
+		
+		// no nextid meaning our term is in last position
+		if( $term_in_level && null === $next_id )
+			$index = $this->set_term_order( $id, $taxonomy, $index+1, true );
+		
+		return $index;
+		
+	}
+	
+	/**
+	 * Set the sort order of a term
+	 * 
+	 * @param int $term_id
+	 * @param int $index
+	 * @param bool $recursive
+	 */
+	private function set_term_order ( $term_id, $taxonomy, $index, $recursive=false ) {
+		
+		global $wpdb;
+		
+		$term_id 	= (int) $term_id;
+		$index 		= (int) $index;
+		
+		update_metadata( 'term', $term_id, 'order', $index );
+		
+		if( ! $recursive ) return $index;
+		
+		$children = get_terms( $taxonomy, "parent=$term_id&menu_order=ASC&hide_empty=0" );
+	
+		foreach ( $children as $term ) {
+			$index ++;
+			$index = $this->set_term_order ( $term->term_id, $index, true );		
+		}
+		
+		return $index;
+	
+	}
+
+}
+
+if( ! function_exists('add_term_ordering_support') ) {
+	function add_term_ordering_support ($taxonomy) {
+		Gecka_Terms_Ordering::add_taxonomy_support($taxonomy);
+	} 
+}
+
+if( ! function_exists('remove_term_ordering_support') ) {
+	function remove_term_ordering_support ($taxonomy) {
+		Gecka_Terms_Ordering::remove_taxonomy_support($taxonomy);
+	} 
+}
+
+if( ! function_exists('has_term_ordering_support') ) {
+	function has_term_ordering_support ($taxonomy) {
+		return Gecka_Terms_Ordering::has_support($taxonomy);
+	} 
+}
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Gecka Terms Ordering ===
+Contributors: Gecka Apps
+Tags: term, terms, category, categories, terms ordering, categories sorting, sort categories, categories order, categories ordering, category order, taxonomy order, taxonomies order
+Requires at least: 3.0
+Tested up to: 3.1.4
+Stable tag: 1.0-beta
+Submenu is a  WordPress plug-in that enhances the WordPress 3.x navigation system.
+
+== Description ==
+
+Have you ever wanted to sort your categories using drag and drop? Have you ever used a plugin that implements some sort of custom taxonomy or custom categories but no drag and drop sorting support?
+
+Then, that plugin is for you. Using it you can enable categories custom ordering using drag and drop. But not only, you can set it to enable custom ordering of any other taxonomy's terms, even taxonomies created by other plugins.
+
+By default, drag and drop sorting is enabled for your posts categories. If you want to order any other taxonomy, go read <a href="http://gecka-apps.com/">the plugin's page on our website</a>.
+
+You would like to support us? Go over <a href="http://gecka-apps.com">at our website</a> and check our great premium plugins.
+
+Want to contribute? That plugin is on <a href="https://github.com/Gecka-Apps/Gecka-Terms-Ordering">GitHub</a>
+
+== Installation ==
+
+1. Download the plugin file
+2. Unzip the file into a folder on your hard drive
+3. Upload `gecka-terms-ordering` folder to the `/wp-content/plugins/` folder on your site
+4. Visit the plugins page in admin and activate it
+
+== Screenshots ==
+
+1. In action
+
+== Changelog ==
+
+= 1.0-beta1 =
+* First release.
